c7bb2b801905a569f7ea35a467363c3567b2de0b
Shelgas <akozlov1997@gmail.com>
2024-04-21 10:04:27 +0300

    Added base Model component

--- src/components/base/Component.ts
+++ src/components/base/Component.ts
@@ -1,11 +1,6 @@
-import {IEvents} from "./events";
-
-/**
- * Базовый компонент
- */
 export abstract class Component<T> {
     protected constructor(protected readonly container: HTMLElement) {
-        // Учитывайте что код в конструкторе исполняется ДО всех объявлений в дочернем классе
+        
     }
 
     // Инструментарий для работы с DOM в дочерних компонентах
