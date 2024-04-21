fec268a6c3d3da82090d2101a6c58882555b6a06
Shelgas <akozlov1997@gmail.com>
2024-04-21 19:01:33 +0300

    Added information to readme file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
-export type CategoryType = 'софт-скил' | 'хард-скил' | 'другое' | 'кнопка' | 'дополнительное';
+export type CategoryType = 'софт-скил' | 'хард-скил' | 'другое' | 'кнопка' | 'дополнительное'; // категории товаров 
 
-export interface IProduct { 
+export interface IProduct {   // интерфейс товаров 
     id: string;
     title: string;
 	description: string; 
@@ -9,38 +9,38 @@ export interface IProduct {
 	price: number | null;
 }
 
-enum PaymentType {
+enum PaymentType { // перечсление способов оплаты заказа
     Online = 'Онлайн',
     UponReceipt = 'При получении'
   }
 
-export interface IContactsForm {
+export interface IContactsForm { // интерфейс формы контактных данных 
 	phone: string;
 	email: string;
 }
 
-export interface IOrderForm {
+export interface IOrderForm { // интерфейс формы с адресом доставки
 	address: string;
 	payment: PaymentType;
 }
 
-export interface IOrder extends IOrderForm, IContactsForm {
-    items: string[]
+export interface IOrder extends IOrderForm, IContactsForm { // интерфейс данных заказа
+    items: string[];
 }
 
-export interface IAppState {
+export interface IAppState { // интерфейс данных приложения
     catalog: IProduct[];
     basket: string[];
     order: IOrder | null;
 }
 
-export interface IBasketItem {
+export interface IBasketItem { // интерфейс элемента корзины
     index: number;
     title: string;
     price: number | null;
 } 
 
-export interface IBasketCard {
+export interface IBasketCard { // интерфейс корзины
 	items: IBasketItem[];
 	total: number; 
 }
